9e428ae332c1af5489e08270d73db77a66ad0dea
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-03-09 22:26:36 -0500

    Completed exercise 3 of chapter 3, target added in makefile.

---
+++ src/chapter_3/3-3.cpp
@@ -0,0 +1,26 @@
+/*
+	Jeffrey Diaz - jeffreydiaz27@gmail.com
+	Chapter 3, exercise 3
+	Page 85
+*/
+#include "std_lib_facilities.h"
+
+int main()
+{
+	// illegal names
+	int char    = 1;
+	int if      = 2;
+	double else = 3;
+	double 2222 = 4;
+	int $x axis = 5;
+
+	// legal names
+	int even    = 6;
+	int odd     = 7;
+	int prime   = 11;
+	char foo    = 'f';
+	double val1 = 8.0;
+	double val2 = 9.0;
+
+	return 0;
+}
