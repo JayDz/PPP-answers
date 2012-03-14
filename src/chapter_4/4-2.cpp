e1d44b346e873c05c05fe2b9823741ef0cc5b707
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-03-14 08:42:18 -0400

    Completed exercise 2 of chapter 4, added target to makefile

---
+++ src/chapter_4/4-2.cpp
@@ -0,0 +1,27 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	vector<double> temps;
+	double temp;
+	while (cin >> temp)
+		temps.push_back(temp);
+
+	double sum = 0;
+	for (int i = 0; i < temps.size(); ++i) 
+		sum += temps[i];
+	
+	cout << "Average temp: " << sum / temps.size() << endl;
+
+	sort(temps.begin(), temps.end());
+
+	if (temps.size() % 2 == 0) {
+		double mid = ((temps[temps.size() / 2]) +
+		             (temps[temps.size() / 2 - 1])) / 2.0;
+		cout << "Median temp is: " << mid << endl;
+	}
+	else 
+		cout << "Median temp is: " << temps[temps.size() / 2] << endl;
+	
+	return 0;
+}
