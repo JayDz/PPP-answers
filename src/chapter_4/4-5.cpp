cdeba594abf9462e7954031c54cc357442549391
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-03-15 20:42:29 -0400

    Completed exercise 6 of chapter 4, and cleaned up code in 4-5.cpp.

--- src/chapter_4/4-5.cpp
+++ src/chapter_4/4-5.cpp
@@ -13,25 +13,30 @@ int main()
 
 	cout << "Please enter three arguments (x y +): ";
 	cin >> left_op >> right_op >> operation;
-
-	if (operation == '+') 
+	
+	switch (operation) {
+	case '+': 
 		cout << "The sum of " << left_op << " and " << right_op
-		     << " is: " << left_op + right_op << endl;
-	else if (operation == '-')
+		     << " is: " << left_op + right_op << endl;\
+	    break;
+	case '-':
 		cout << "The difference of " << left_op << " and " << right_op
 		     << " is: " << left_op - right_op << endl;
-	else if (operation == '*')
+		break;
+	case '*':
 		cout << "The product of " << left_op << " and " << right_op
 		     << " is: " << left_op * right_op << endl;
-	else if (operation == '/') {
+		break;
+	case '/':
 		if (right_op == 0)
 			cout << "Cannot divide by zero!" << endl;
 		else
 			cout << "The quotient of " << left_op << " and " << right_op
 			     << " is: " << left_op / right_op << endl;
-	}
-	else 
+		break;
+	default: 
 		cout << "Unknown operation." << endl;
-
+		break;
+	}
 	return 0;
 }
--- src/chapter_4/4-6.cpp
+++ src/chapter_4/4-6.cpp
@@ -19,16 +19,39 @@ int main()
 	values[8] = "eight";
 	values[9] = "nine";
 
-	cout << "Enter a digit (0-9) to convert to its spelled equivalent: ";
-	int digit;
-	cin >> digit;
+	cout << "Enter a digit (0-9) or spelled out digit to convert to its opposite equivalent: ";
+	string digit;
+	while(cin >> digit) {
+			int index = -1;
+			for (int i = 0; i < values.size(); ++i)
+				if (digit == values[i])
+					index = i;
+				
+			// if index equals -1 it means user input was in digit form.
+			// On the other hand, if index is not equal to -1, input was in spelled-out form
+			// and contains the index of the word we want to output.
+			if (index != -1)
+				cout << digit << " in digit form is: " << index << endl;
+			else {
+				if (digit == "0")      index = 0;
+				else if (digit == "1") index = 1;
+				else if (digit == "2") index = 2;
+				else if (digit == "3") index = 3;
+				else if (digit == "4") index = 4;
+				else if (digit == "5") index = 5;
+				else if (digit == "6") index = 6;
+				else if (digit == "7") index = 7;
+				else if (digit == "8") index = 8;
+				else if (digit == "9") index = 9;
+				else 
+					cout << "Invalid input." << endl;
+				
+				// no need to bounds check, book's header file throws the excetion for us.
+				cout << digit << " in spelled-out form is: " << values[index] << endl;
+			}
 
-	// don't even need to check, std_lib_facilities throws exception for us
-	if (digit < 0 || digit > values.size())
-		cout << "Invalid digit." << endl;
-	else
-		cout << "The spelled-out equivalent of " << digit
-		     << " is: " << values[digit] << endl;
+		cout << "Enter a digit (0-9) or spelled out digit to convert to its opposite equivalent: ";
+	}
 
 	return 0;
 }
