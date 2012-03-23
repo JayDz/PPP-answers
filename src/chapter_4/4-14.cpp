959ad707eaeaa62ac6894d3494a89fa6a5b3d5d3
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-03-22 23:47:03 -0400

    Completed exercise 14 of chapter 4.

---
+++ src/chapter_4/4-14.cpp
@@ -0,0 +1,53 @@
+/*
+	Jeffrey Diaz - jeffreydiaz27@gmail.com
+	Chapter 4 exercise 14
+	Page 128
+*/
+#include "std_lib_facilities.h"
+
+vector<int> primes;
+
+void find_primes()
+	// Find prime numbers using the Sieve of Eratosthenes method.
+{
+	for (int i = 0; i < primes.size(); ++i) {
+		int found = 0;
+		int p;
+		if (primes[i] != 0) {
+			found = 1;
+			p = primes[i];
+		}
+
+		int pp = p;
+		if (found == 1) {
+			while ((i + pp) <= (primes.size() - 1)) {
+				primes[i + pp] = 0;
+				pp += p;
+			}
+		}
+	}
+}
+
+int main()
+{
+	cout << "I will find all prime numbers between 1 and max, please enter a max: ";
+	int max;
+	cin >> max;
+	if (max > 0) {
+		// Generate list of numbers from 2 to max.
+		for (int i = 2; i <= max; ++i)
+			primes.push_back(i);
+
+		find_primes(); // Finding primes in range(1,100).
+		
+		// List prime numbers found.
+		for (int i = 0; i < primes.size(); ++i) {
+			if (primes[i] != 0) 
+				cout << primes[i] << endl;
+		}
+	}
+	else
+		cout << "Max must be greater than one." << endl;
+
+	return 0;
+}
