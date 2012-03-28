5f92ea1862722c54ba52f39bec441f1e61659d7f
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-03-27 23:47:51 -0400

    Completed exercise 4 of chapter 5.

---
+++ src/chapter_5/5-4.cpp
@@ -0,0 +1,28 @@
+/*
+	Jeffrey Diaz - jeffreydiaz27@gmail.com
+	Chapter 5 exercise 4
+	Page 168
+*/
+#include "std_lib_facilities.h"
+
+double ctok(double c)	// converts Celsius to Kelvin
+{
+
+	const double min_c = -273.15;
+	if (c <= min_c) error("The lowest temperature that can be reached is: -273.15 Celsius.\n");
+	
+	const double kelvin_factor = 273.15;
+	double k = c + kelvin_factor;
+
+	return k;
+}
+
+int main()
+{
+	double c = 0;
+	cin >> c;
+	double k = ctok(c);
+	cout << k << endl;
+
+	return 0;
+}
