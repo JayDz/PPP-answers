757568796a7bda2ee5e84dda8d5f66ba2ba9ac08
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-05-10 02:53:25 -0400

    Removed uncovered facilities from 4-21.cpp.

--- src/chapter_4/4-21.cpp
+++ src/chapter_4/4-21.cpp
@@ -10,40 +10,37 @@ int main()
 	vector<string> names;
 	vector<int> scores;
 	
-	int loop_broke = 0; // Solves the problem of "No more" causing an infinite loop.
-
 	string str_val;
 	int int_val;
-	cout << "Please enter name-and-value pairs (such as Joe 1) where name is unique. Enter No more to end input loop: \n";
-	while (cin >> str_val >> int_val) {
-		int name_has_been_seen = 0;
-		for (int i = 0; i < names.size(); ++i) 
-			if (str_val == names[i])
-				name_has_been_seen = 1;
-		
-		if (name_has_been_seen == 0) {
-			if (int_val == -1337) 
-				cout << int_val << " cannot be recorded, it has special meaning to program." << endl;
-			else {
-				names.push_back(str_val);
-				scores.push_back(int_val);
-			}
+	cout << "Please enter name-and-value pairs (such as Joe 1) where name is unique. Enter NoName 0 to end input loop:\n";
+	
+	int continue_loop = 1;
+	while (continue_loop == 1) {
+		cin >> str_val >> int_val;
+
+		if (str_val == "NoName") {
+			if (int_val == 0)
+				continue_loop = 0;
+			else
+				cout << "Sorry, the only acceptable value for NoName is 0." << endl;
 		}
 		else {
-			cout << "A name cannot be entered twice!" << endl;
-			loop_broke = 1;
-			break;
+			int name_has_been_seen = 0;
+			for (int i = 0; i < names.size(); ++i)
+				if (str_val == names[i])
+					name_has_been_seen = 1;
+					
+			if (name_has_been_seen == 0) {
+				if (int_val == -1337) 
+					cout << int_val << " cannot be recorded, it has special meaning to program." << endl;
+				else {
+					names.push_back(str_val);
+					scores.push_back(int_val);
+				}
+			}
+			else
+				cout << "A name cannot be entered twice!" << endl;
 		}
-	}
-	
-	// There are only two ways to exit the previous loop:
-	// 1. User entered the same name twice. If so loop_broke will equal 1.
-	// 2. User entered "No more" to end the input loop, loop_broke was then not set to 1. 
-	// If option 2 occured, cin is in bad state. In bad state cin does not prompt, causing an infinite loop.
-	if (loop_broke == 0) {
-		cin.clear(); // Had to look this up.
-		string junk;
-		cin >> junk;
 	}
 
 	if (names.size() > 0) {
