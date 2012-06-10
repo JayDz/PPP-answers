1212c362cd128966e61bba4ac3a6a52aaff69733
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-06-09 20:37:33 -0400

    Removed dead code.

--- src/chapter_9/9-10.cpp
+++ src/chapter_9/9-10.cpp
@@ -5,14 +5,8 @@
 #include "std_lib_facilities.h"
 
 bool leapyear(int y)
-{
-	int last_digit = y % 10;
-	y /= 10;
-	int next_last  = y % 10;
-
-	int value = 10*next_last+last_digit;
-	
-	if ((value % 4 == 0) && (value % 100 != 0) && (value % 400 != 0))
+{	
+	if ((y % 4 == 0) && (y % 100 != 0) && (y % 400 != 0))
 		return true;
 	return false;
 }
