a0e2d7bd833654f088e1deb885ecff52c76d070b
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-06-23 01:50:53 -0400

    Completed exercise 1 of chapter 12.

---
+++ src/chapter_12/12-1.cpp
@@ -0,0 +1,37 @@
+/*
+	chapter 12 exercise 1
+	page 434
+*/
+#include "std_lib_facilities.h" //for exception type.
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main()
+{
+	try {
+		Simple_window window(Point(100,100),600,400,"Exercise one");
+
+		Rectangle r(Point(100,100),200,100);
+		r.set_color(Color::blue);
+
+		Polygon p;
+		p.add(Point(300,100));
+		p.add(Point(500,100));
+		p.add(Point(500,200));
+		p.add(Point(300,200));
+		p.set_color(Color::red);
+
+		window.attach(r);
+		window.attach(p);
+
+		window.wait_for_button();
+	}
+	catch(exception& e) {
+		cerr << e.what() << endl;
+		return 1;
+	}
+	catch(...) {
+		return 2;
+	}
+	return 0;
+}
