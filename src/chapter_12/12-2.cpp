24a97dd98e8e8125b8753a0b8a5d65fea6ece6e8
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2012-06-23 01:53:50 -0400

    Completed exercise 2 of chapter 12.

---
+++ src/chapter_12/12-2.cpp
@@ -0,0 +1,30 @@
+/*
+	chapter 12 exercise 2
+	page 434
+*/
+#include "std_lib_facilities.h"
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main()
+{
+	try {
+		Simple_window window(Point(100,100),600,400,"Exercise 2");
+
+		Rectangle rect(Point(250,150),100,30);
+		Text label(Point(275,170),"Howdy!");
+
+		window.attach(rect);
+		window.attach(label);
+
+		window.wait_for_button();
+	}
+	catch(exception& e) {
+		cerr << e.what() << endl;
+		return 1;
+	}
+	catch(...) {
+		return 2;
+	}
+	return 0;
+}
